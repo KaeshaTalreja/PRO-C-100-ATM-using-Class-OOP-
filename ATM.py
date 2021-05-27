3908c3c3251789c4b4fab5f1d418a3763717228d
KaeshaTalreja <65214092+KaeshaTalreja@users.noreply.github.com>
2021-05-27 22:47:57 +0530

    Add files via upload

--- ATM.py
+++ ATM.py
@@ -1,3 +1,7 @@
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# python3 get-pip.py
+# pip3 install howdoi
+
 class ATM(object):
   def __init__(self,cardNo,pinNo,custName):
     self.cardNo=cardNo
